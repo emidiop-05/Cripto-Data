ad95c5472076ba16b8543765150c9894bf93317a
Emidio Pedro <emidiop44@gmail.com>
2025-12-04 20:08:48 +0100

    first img for project

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import LogoLarge from "./assets/Large-Logo.png";
+import LogoMedium from "./assets/Medium-Logo.png";
+import LogoSmall from "./assets/Small-Logo.png";
+
+function App() {
+  return (
+    <div>
+      <img src={LogoLarge} alt="" />
+      <img src={LogoMedium} alt="" />
+      <img src={LogoSmall} alt="" />
+    </div>
+  );
+}
+
+export default App;
